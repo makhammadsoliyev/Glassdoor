0d20d8419c5243f9b43df2cad1d6029dd8531dd6
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-11 09:16:38 +0500

    Created Entities

---
+++ Glassdoor.Domain/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glassdoor.Domain.Entities;
+
+public class Address
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
