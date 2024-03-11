88810fa905b88165750b9887392512d7e8b6628b
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-11 09:34:58 +0500

    Created Migrations

---
+++ Glassdoor.DataAccess/Contexts/GlassdoorDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Glassdoor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glassdoor.DataAccess.Contexts;
+
+public class GlassdoorDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localDB\\MSSQLLocalDb; Database=GlassdoorDB");
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Resume> Resumes { get; set; }
+}
