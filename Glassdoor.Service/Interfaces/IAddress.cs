8a5d906d1d1b898953162b49e9064ce7de3c895a
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-11 10:57:24 +0500

    Created Interfaces And Services In Glassdoor.Service

---
+++ Glassdoor.Service/Interfaces/IAddress.cs
@@ -0,0 +1,18 @@
+﻿using Glassdoor.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glassdoor.Service.Interfaces;
+
+public interface IAddress
+{
+    Task<Address> CreateAsync(Address model);
+    Task<Address> UpdateAsync(long id, Address model);
+    Task<bool> DeleteAsync(long id);
+    Task<Address> GetByIdAsync(long id);
+    Task<IEnumerable<Address>> GetAllAsEnumerable();
+    Task<IQueryable<Address>> GetAllAsQueyable();
+}
---
+++ Glassdoor.Service/Interfaces/IUser.cs
@@ -0,0 +1,18 @@
+﻿using Glassdoor.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glassdoor.Service.Interfaces;
+
+public interface IUser
+{
+    Task<User> CreateAsync(User model);
+    Task<User> UpdateAsync(long id, User model);
+    Task<bool> DeleteAsync(long id);
+    Task<User> GetByIdAsync(long id);
+    Task<IEnumerable<User>> GetAllAsEnumerable();
+    Task<IQueryable<User>> GetAllAsQueyable();
+}
