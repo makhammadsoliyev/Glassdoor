ce8a2b29b75331bf9783548b29c25a8308df7e53
xudaykulov-xamidullo <xudaykulov123@gmail.com>
2024-03-12 09:37:20 +0500

    Created Models Job And Company

--- Glassdoor.DataAccess/Contexts/GlassdoorDbContext.cs
+++ Glassdoor.DataAccess/Contexts/GlassdoorDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Glassdoor.Domain.Entities;
+﻿using Glassdoor.DataAccess.Configurations;
+using Glassdoor.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,7 @@ public class GlassdoorDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocaldb;Database=GlassdoorDB; Trusted_Connection=True;");
+        var connectionString = $"Host={Constants.HOST};Port={Constants.PORT};Database={Constants.DATABASE};User Id={Constants.USER};Password={Constants.PASSWORD};";
+        optionsBuilder.UseNpgsql(connectionString);
     }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Address> Addresses { get; set; }
-    public DbSet<Resume> Resumes { get; set; }
 }
